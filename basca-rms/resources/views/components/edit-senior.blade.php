bd0aecc10dd648faf80bf004e8b87d14d491962c
Jimbert Lucero <jimbertlucero@aldersgate.edu.ph>
2026-08-20 21:07:18 +0800

    fixed edit page dropdown

--- basca-rms/resources/views/components/edit-senior.blade.php
+++ basca-rms/resources/views/components/edit-senior.blade.php
@@ -5,7 +5,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <x-browse-top />
-    @vite(['resources/css/app.css', 'resources/js/app.js'])
+    @vite(['resources/css/app.css', 'resources/js/app.js', 'resources/js/add-senior.js'])
     <title>BASCA-RMS - Edit Senior Citizen</title>
 </head>
 
